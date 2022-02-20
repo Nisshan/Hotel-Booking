41fcc22bd1fe9afa6ee396ae6ef8f64e20dea6d5
nisshan <timsinanishan1@gmail.com>
2022-02-20 20:27:59 +0545

    setting up phpfixer wip

--- .php_cs.dist.php
+++ .php_cs.dist.php
@@ -2,7 +2,11 @@
 
 $finder = Symfony\Component\Finder\Finder::create()
     ->in([
-        __DIR__ . '/src',
+        __DIR__ . '/app',
+        __DIR__ . '/config',
+        __DIR__ . '/database',
+        __DIR__ . '/resources',
+        __DIR__ . '/routes',
         __DIR__ . '/tests',
     ])
     ->name('*.php')
